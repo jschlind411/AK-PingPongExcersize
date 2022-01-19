5d6812fa438806397ff7f1e2fcb3d6aa6ca1b6eb
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2022-01-19 11:45:19 -0700

    initial commit

---
+++ JS-AK-PingPongExcersize/JS-AK-PingPongExcersize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JS_AK_PingPongExcersize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
