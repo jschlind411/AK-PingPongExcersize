fd89f29ffd671fa3a632584845a81275f53a10e2
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2022-01-19 14:56:42 -0700

    made test pass

--- JS-AK-PingPongExcersize/PingPongModels/TicTacToe.cs
+++ JS-AK-PingPongExcersize/PingPongModels/TicTacToe.cs
@@ -4,9 +4,18 @@ namespace PingPongModels
 {
     public class TicTacToe
     {
+        private bool hasMoved = false;
         public bool Play(char character, int gridLocation)
         {
-            return true;
+            if(hasMoved == false)
+            {
+                hasMoved = true;
+                return hasMoved;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
