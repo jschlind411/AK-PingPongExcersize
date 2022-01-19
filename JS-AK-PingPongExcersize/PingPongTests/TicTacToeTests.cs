d04df439833212db77f643721ad0d56f4a9098a9
Ashley Kinsel <ashley.kinsel@ruralsourcing.com>
2022-01-19 14:12:17 -0700

    Created "an_Create_TicTacToe_Object" Test

---
+++ JS-AK-PingPongExcersize/PingPongTests/TicTacToeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PingPongTests
+{
+    public class TicTacToeTests
+    {
+        [Fact]
+        public void Can_Create_TicTacToe_Object()
+        {
+            var TicTacToe = new TicTacToe();
+        }
+    }
+}
