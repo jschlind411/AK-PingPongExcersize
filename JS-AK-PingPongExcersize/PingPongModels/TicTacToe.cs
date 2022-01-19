cae1b2fa23c3ed5dc5c2c4b0204325c91a5c36f6
Jeff Schlindwein <jeff.schlindwein@gmail.com>
2022-01-19 14:19:19 -0700

    Made pass

--- JS-AK-PingPongExcersize/PingPongModels/TicTacToe.cs
+++ JS-AK-PingPongExcersize/PingPongModels/TicTacToe.cs
@@ -2,7 +2,7 @@
 
 namespace PingPongModels
 {
-    public class Class1
+    public class TicTacToe
     {
     }
 }
--- JS-AK-PingPongExcersize/PingPongTests/TicTacToeTests.cs
+++ JS-AK-PingPongExcersize/PingPongTests/TicTacToeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using PingPongModels;
 using Xunit;
 
 namespace PingPongTests
