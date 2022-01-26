20a0636c2a8d214d3dd78b40b74816725bc72dae
Ashley Kinsel <ashley.kinsel@ruralsourcing.com>
2022-01-26 15:13:23 -0700

    Failed compiling

---
+++ FizzBuzz_PingPong_1_26_2022/PingPongTests_FizzBuzz/FizzBuzz_Game_Tests.cs
@@ -0,0 +1,15 @@
+﻿using PingPong_Models;
+using System;
+using Xunit;
+
+namespace FizzBuzz_Tests
+{
+    public class FizzBuzz_Game_Tests
+    {
+        [Fact]
+        public void FizzBuzz_Game_NotNull()
+        {
+            var result = new FizzBuzzGame();
+        }
+    }
+}
