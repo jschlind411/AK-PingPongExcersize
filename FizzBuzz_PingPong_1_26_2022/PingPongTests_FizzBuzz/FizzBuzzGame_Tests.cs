14e52b10307a7db0cbffe888ae9a8a870ec4ed31
Ashley Kinsel <ashley.kinsel@ruralsourcing.com>
2022-01-27 14:46:13 -0700

    Fizzbuzzer should exist

--- FizzBuzz_PingPong_1_26_2022/PingPongTests_FizzBuzz/FizzBuzzGame_Tests.cs
+++ FizzBuzz_PingPong_1_26_2022/PingPongTests_FizzBuzz/FizzBuzzGame_Tests.cs
@@ -12,6 +12,23 @@ namespace FizzBuzz_Tests
             fizzBuzz = new FizzBuzzGame();
         }
 
+        public class FizzBuzzerTests
+        {
+            private FizzBuzzGame fizzBuzz;
+
+            public FizzBuzzerTests()
+            {
+                fizzBuzz = new FizzBuzzGame();
+            }
+
+            [Fact]
+            public void FizzBuzzer_IsCreated_WithinFizzBuzzGame()
+            {
+                Assert.NotNull(fizzBuzz.fizzBuzzer);
+            }
+        }
+
+
         [Fact]
         public void FizzBuzzGame_CanBeCreated()
         {
@@ -24,6 +41,14 @@ namespace FizzBuzz_Tests
             var result = fizzBuzz.Verify("", 0);
 
             Assert.IsType<bool>(result);
+        }
+
+        [Fact]
+        public void Verify_GivenAValidGuess_ReturnsTrue()
+        {
+            var result = fizzBuzz.Verify("", 0);
+
+            Assert.True(result);
         }
     }
 }
