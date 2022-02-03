923be3813224e2ae9dcaf5ff76f0590ad718e4a0
foxguy411 <jeff.schlindwein@gmail.com>
2022-02-03 15:09:13 -0700

    Fixed

--- FizzBuzz_PingPong_1_26_2022/PingPong_Models/FizzBuzzGame.cs
+++ FizzBuzz_PingPong_1_26_2022/PingPong_Models/FizzBuzzGame.cs
@@ -34,7 +34,10 @@ namespace FizzBuzz_Models
 
         public void CreatePlayer(string name)
         {
-            Players.Add(new Player());
+            Players.Add(new Player() 
+            {
+                Name = "Name"
+            });
         }
     }
 }
