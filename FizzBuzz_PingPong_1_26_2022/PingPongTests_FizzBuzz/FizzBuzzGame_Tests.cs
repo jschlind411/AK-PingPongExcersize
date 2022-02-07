117f35afb4cdc5a7a387e5e0b07bfd6317f34a62
Ashley Kinsel <ashley.kinsel@ruralsourcing.com>
2022-02-07 14:32:33 -0700

    AddPlayer twice failed

--- FizzBuzz_PingPong_1_26_2022/PingPongTests_FizzBuzz/FizzBuzzGame_Tests.cs
+++ FizzBuzz_PingPong_1_26_2022/PingPongTests_FizzBuzz/FizzBuzzGame_Tests.cs
@@ -8,29 +8,35 @@ namespace FizzBuzz_Tests
 {
     public class FizzBuzzGame_Tests
     {
+        FizzBuzzGame game;
+
+        public FizzBuzzGame_Tests()
+        {
+            game = new FizzBuzzGame();
+        }
+
         [Fact]
         public void FizzBuzzGame_CanBeCreated()
         {
-            var game = new FizzBuzzGame();
-
             Assert.NotNull(game);
         }
 
         [Fact]
         public void PlayersList_IsInFizzBuzzGame()
         {
-            var game = new FizzBuzzGame();
-
             Assert.IsType<List<Player>>(game.Players);
         }
 
         [Fact]
         public void AddPlayer_AddsPlayer_ToPlayersList()
         {
-            var game = new FizzBuzzGame();
             game.AddPlayer("Bill");
 
             Assert.True(game.Players.Count == 1);
+
+            game.AddPlayer("Ted");
+
+            Assert.True(game.Players.Count == 2);
         }
 
     }
