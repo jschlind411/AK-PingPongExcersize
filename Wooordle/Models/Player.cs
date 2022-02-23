6020e38494154ae3c737323abe63667eb392679f
foxguy411 <jeff.schlindwein@gmail.com>
2022-02-23 14:48:15 -0700

    fixed per unit test request

--- Wooordle/Models/Player.cs
+++ Wooordle/Models/Player.cs
@@ -4,7 +4,14 @@ namespace Models
 {
     public class Player
     {
-        public static string Name { get; } = string.Empty;
+        public Player() : this(string.Empty) { }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public static string Name { get; private set; } = string.Empty;
         public static int Score { get; } = 0;
     }
 }
