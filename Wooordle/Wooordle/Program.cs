afb7e216e57ac8113204c86eb5cb2955d46a7311
foxguy411 <jeff.schlindwein@gmail.com>
2022-02-23 14:08:56 -0700

    Initial skeleton and project checkin

---
+++ Wooordle/Wooordle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wooordle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
