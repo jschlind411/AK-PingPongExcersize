3d2afd2a93db6388182e8ee4e9843557ca1b558c
foxguy411 <jeff.schlindwein@gmail.com>
2022-03-02 14:06:46 -0700

    added new test

---
+++ Wooordle/WooordleTests/WordsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WooordleTests
+{
+    public class WordsTests
+    {
+        [Fact]
+        public void CanCreateWordsObject()
+        {
+            Words words = new Words();
+        }
+
+    }
+}
