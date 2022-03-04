104b18528427e03368092e16288c1d5f2219b134
Ashley Kinsel <ashley.kinsel@ruralsourcing.com>
2022-03-04 15:06:03 -0700

    UsedWords exists

--- Wooordle/Models/Words.cs
+++ Wooordle/Models/Words.cs
@@ -10,6 +10,8 @@ namespace Models
     {
         protected List<string> WordList { get; set; }
 
+        protected List<string> UsedWords { get; set; };
+
         protected string CurrentWord { get; set; } = "tests";
 
         public Words()
